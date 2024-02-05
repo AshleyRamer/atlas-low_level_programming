fdf9c87088ac319c59d1c32c835cfe1bb39a12b7
AshleyRamer <ashleyramer7@gmail.com>
2024-02-05 22:24:21 +0000

    fixed config

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @n: bytes to be changed
+ * @s: starting address of memory to be filled
+ * @b: desired value
+ * Return: changed array w/ new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
