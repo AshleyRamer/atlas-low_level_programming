ac9f47ecb1a20d8e4318ad56fe4fb7b613828e33
AshleyRamer <ashleyramer7@gmail.com>
2024-02-15 16:36:43 +0000

    factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a number
+ * @n: factoirial number
+ * Return: factorial n
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}	
+	return (n * factorial(n - 1));
+}
