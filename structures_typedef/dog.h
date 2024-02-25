d9237b769622b59ca682eabe9d5ddbb53e24409a
AshleyRamer <ashleyramer7@gmail.com>
2024-02-25 19:33:52 +0000

    hate this

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Mr. Poppy Dog's info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
