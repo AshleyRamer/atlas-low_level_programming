e3396194e7af27912ca826b9ae1a533fc289e205
JustAgust <charles.sparks@atlasschool.com>
2024-02-29 17:40:29 +0000

    created strlen.

--- printf/_strlen.c
+++ printf/_strlen.c
@@ -2,4 +2,16 @@
 /**
  * _strlen - a function that returns the lenght of string
  * @s: parameter
- * Return: 
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
