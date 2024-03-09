59f31f9e504b579968c64c29e99c21ff352dff10
AshleyRamer <ashleyramer7@gmail.com>
2024-03-09 00:24:11 +0000

    0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length
+ * @next: pointer to next node
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
